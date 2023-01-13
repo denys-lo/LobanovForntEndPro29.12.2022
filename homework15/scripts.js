e0795f0f8e671451e57133f0981596dc9982b80f
denys-lo <div.p.div@gmail.com>
2023-01-13 13:35:16 +0100

    FIX - HW15. Recursive reduction to a power

--- homework15/scripts.js
+++ homework15/scripts.js
@@ -1,17 +1,10 @@
-// ДЗ 15. Рекурсивне зведення в ступінь
-// Реалізувати рекурсивну функцію, яка зводить число в ступінь.
-// Число, яке треба піднести до ступеню,
-// передається як перший аргумент у функції;
-// Ступінь передається як другий аргумент у функціюpow (num, degree).
 const getNumber = +prompt('Enter any number: ');
 const getDegree = +prompt('Enter degree: ');
 
 function pow(num, degree) {
-  const numberForPow = getNumber;
   if (degree > 1) {
-    pow(num * numberForPow, degree - 1);
-  } else {
-    console.log(num);
+    return pow(num * getNumber, degree - 1);
   }
+  return num;
 }
-pow(getNumber, getDegree);
+console.log(pow(getNumber, getDegree));
